94d12c5127d6deb7acbb13da82b66c7d39f54271
ndneighbor <asara019@fiu.edu>
2022-02-09 23:08:46 -0500

    everything

---
+++ app/src/App.tsx
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react";
+import {
+  Box,
+  Badge,
+  Image,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Flex,
+} from "@chakra-ui/react";
+import { Formik, Form, Field } from "formik";
+import "./App.css";
+
+interface Album {
+  id: number;
+  title: string;
+  price: number;
+  artist: string;
+  image: string;
+}
+
+const AlbumBlock = (album: Album) => {
+  return (
+    <div key={album.id}>
+      <Box
+        m="4"
+        maxW="sm"
+        borderWidth="1px"
+        borderRadius="lg"
+        overflow="hidden"
+      >
+        <Box p="6">
+          <Box display="flex" alignItems="center">
+            <Image
+              borderRadius="lg"
+              boxSize="100px"
+              objectFit="cover"
+              src={album.image}
+            />
+            <Box m={2}>
+              <Box display="flex">
+                <Badge borderRadius="full" px="2" colorScheme="teal">
+                  {album.price}
+                </Badge>
+                <Box
+                  color="gray.500"
+                  fontWeight="semibold"
+                  letterSpacing="wide"
+                  fontSize="xs"
+                  textTransform="uppercase"
+                  ml="2"
+                >
+                  {album.artist}
+                </Box>
+              </Box>
+
+              <Box
+                mt="1"
+                fontWeight="semibold"
+                as="h4"
+                lineHeight="tight"
+                display="flex"
+                textAlign="initial"
+              >
+                {album.title}
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+function App() {
+  const [data, setData] = useState([]);
+  const [isSubmitting, setSubmitting] = useState(false);
+
+  function fetchData() {
+    fetch("http://127.0.0.1:8080/albums")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const albums = data.map((album: Album) => <AlbumBlock {...album} />);
+
+  function validateField(value: string) {
+    let error;
+    if (!value) {
+      error = "Value is required";
+    }
+    return error;
+  }
+
+  return (
+    <div className="App">
+      <Box bg="teal" w="100%" p="4" color="white">
+        Album Store Admin Panel
+      </Box>
+      <Flex>
+        <Box>{albums}</Box>
+        <Box w="50%" m="4">
+          <Formik
+            initialValues={{
+              title: "Album Name",
+              artist: "Name",
+              image: "",
+              price: 0,
+            }}
+            onSubmit={(values, actions) => {
+              fetch("http://127.0.0.1:8080/albums", {
+                method: "POST",
+                body: JSON.stringify({ id: "4", ...values }),
+              });
+              actions.setSubmitting(false);
+              fetchData();
+            }}
+          >
+            {(props) => (
+              <Form>
+                <Field name="title" validate={validateField}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <FormLabel htmlFor="title">Album Title</FormLabel>
+                      <Input {...field} id="title" placeholder="Album Title" />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="artist" validate={validateField}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <FormLabel htmlFor="artist">Artist</FormLabel>
+                      <Input {...field} id="artist" placeholder="Artist" />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field name="image" validate={validateField}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <FormLabel htmlFor="image">Image</FormLabel>
+                      <Input {...field} id="image" placeholder="Album Image" />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+                <Field type="number" name="price" validate={validateField}>
+                  {({ field, form }) => (
+                    <FormControl
+                      isInvalid={form.errors.name && form.touched.name}
+                    >
+                      <FormLabel htmlFor="price">Price</FormLabel>
+                      <Input
+                        type="number"
+                        {...field}
+                        id="price"
+                        placeholder="Album Price"
+                      />
+                      <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                    </FormControl>
+                  )}
+                </Field>
+
+                <Button
+                  mt={4}
+                  colorScheme="teal"
+                  isLoading={props.isSubmitting}
+                  disabled={isSubmitting}
+                  type="submit"
+                >
+                  Submit
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Flex>
+    </div>
+  );
+}
+
+export default App;
---
+++ backend/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	cors "github.com/rs/cors/wrapper/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(cors.Default())
+	// To be able to send tokens to the server.
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{"hello": "world"})
+	})
+	router.GET("/albums", getAlbums)
+	router.POST("/albums", postAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+
+	router.Run("localhost:8080")
+}
+
+// album type has data about the record
+
+type album struct {
+	ID     string  `json:"id"`
+	Image  string  `json:"image"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99, Image: "https://upload.wikimedia.org/wikipedia/en/6/68/John_Coltrane_-_Blue_Train.jpg"},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99, Image: "https://upload.wikimedia.org/wikipedia/en/c/ca/Jeru_%28album%29.jpg"},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99, Image: "https://upload.wikimedia.org/wikipedia/en/d/d8/Sarah_Vaughan.jpg"},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	var newAlbum album
+
+	// Call BindJSON to bind the received JSON to
+	// newAlbum.
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Add the new album to the slice.
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+// getAlbumByID locates the album whose ID value matches the id
+// parameter sent by the client, then returns that album as a response.
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of albums, looking for
+	// an album whose ID value matches the parameter.
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// Add delete later
+// func deleteAlbumByID(x *gin.Context) {
+// 	id := c.Param("id")
+
+// 	intVar := strconv.Atoi(id)
+
+// 	ablums[intVar-1]
+// }
